0265721dd103749d851fabf2f29bc1357fdae723
Lyazii Christopher <lyaziichristopher@FVFGW173Q05N.local>
2022-02-01 01:36:38 +0530

    sorted and displayed

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-let randomNum=generateRandomSequence();
-console.log(randomNum);
-
 let container=document.getElementById("container");
-randomNum.forEach((item)=>{
-    let column=document.createElement("div");
-    item=item/1000*60;
-    column.style.height=item+"vh";
-    column.className="bar";
-    container.append(column);
+let buttonRandom=document.getElementById("btn-random");
+let buttonSort=document.getElementById("btn-sort");
+
+let randomNum=generateRandomSequence();
+display();
+// console.log(randomNum);
+
+buttonRandom.addEventListener("click",()=>{
+    randomNum=generateRandomSequence();
+    display();
 })
+buttonSort.addEventListener("click",()=>{
+    randomNum.sort((a,b)=>a-b);
+    // console.log(randomNum);
+    display();
+})
+
+function display(){
+    // container.innerHTML="";
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    randomNum.forEach((item)=>{
+        let column=document.createElement("div");
+        item=item/1000*60;
+        column.style.height=item+"vh";
+        column.className="bar";
+        container.append(column);
+    })
+}
 
 function generateRandomSequence(n=100){
     let arr=[];
